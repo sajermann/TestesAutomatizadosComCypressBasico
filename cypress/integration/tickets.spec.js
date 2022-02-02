0767fc020623ac1a74ef1552e5220ea165f8d79d
Bruno Sajermann <contato@sajermann.com>
2022-02-02 07:40:28 -0300

    Video 2 Complete

---
+++ cypress/integration/tickets.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+
+describe("Tickets", () => {
+  beforeEach(() => cy.visit('https://ticket-box.s3.eu-central-1.amazonaws.com/index.html'))
+  it("Has 'Ticketbox' header's heading", ()=>{});
+})
