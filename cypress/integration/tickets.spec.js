48ec0f28e230ad44b08a09a0fbdc057381b9549a
Bruno Sajermann <contato@sajermann.com>
2022-02-03 21:39:54 -0300

    Video 4 Complete

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -12,12 +12,16 @@ Cypress.on(
 describe("Tickets", () => {
   beforeEach(() => cy.visit('https://ticket-box.s3.eu-central-1.amazonaws.com/index.html'));
 
-  it.only("Fills all text input fields", ()=>{
+  it("Fills all text input fields", ()=>{
     cy.get("#first-name").type("Bruno")
     cy.get("#last-name").type("Sajermann")
     cy.get("#email").type("contato@sajermann.com")
     cy.get("#requests").type("Vegetarian")
     cy.get("#signature").type("Bruno S.")
+  })
+
+  it.only("Select two tickets", ()=>{
+    cy.get("#ticket-quantity").select(2)
   })
 
   it("Has 'Ticketbox' header's heading", ()=>{});
